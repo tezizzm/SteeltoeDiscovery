d9e4ed7e175c881008e8096440959d66d698dd00
tezizzm <martezkillens@gmail.com>
2020-10-06 22:25:52 -0700

    Adding discovery apps

---
+++ discovery2x/Program.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
+using Steeltoe.Discovery.Client;
+using Steeltoe.Extensions.Configuration.CloudFoundry;
+
+namespace discovery2x
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                })
+                .AddCloudFoundry()
+                .AddServiceDiscovery();
+    }
+}
